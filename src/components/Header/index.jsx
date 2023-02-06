e04226da73cb88766e31aeb34ec4123dfd3e2804
Tiago Bertoldo <berto.hlb2@gmail.com>
2023-02-06 10:53:53 +0100

    80% header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,80 @@
 import '../Header/Style/desktop.scss'
+import '../Header/Style/resposive.scss'
+import  moveEffect from '../Header/assets/coverr-coding-technology-5974-1080p.mp4'
+import imgProfil from '../Header/assets/img-profil.png'
+import { AiFillLinkedin } from 'react-icons/ai';
+import {AiFillGithub} from 'react-icons/ai'
+
 
 export default  function Header() {
     return (
-       <section className="header-nav">
-            Hello
-       </section>
+       <header id='header'>
+          <div className="container-movie">
+               <video  className='move-style' autoPlay muted loop>
+                    <source className='testess' src={moveEffect} type="video/mp4" />
+               </video>
+          </div>
+          <div className="container-header">
+              <nav className='container-menu-nav'>
+                    <div className="container-burguer-btn">
+                         <div className="burguer"></div>
+                         <div className="burguer"></div>
+                         <div className="burguer"></div>
+                    </div>
+                    <ul>
+                        
+                         <li>
+                              <a href="#header">Home</a>
+                         </li>
+                         <li>
+                              <a href="#header">Competences</a>
+                         </li>
+                         <li>
+                              <a href="#header">Portifolio</a>
+                         </li>
+                         <li>
+                              <a href="#header">À propos</a>
+                         </li>
+                         <li>
+                              <a href="#header">Contact</a>
+                         </li>
+                    </ul>
+              </nav>
+              <section className="container-info-header">
+                    <div className="container-info-header__left">
+                              <div className="container-info-header__reseaux">
+                                   <span>
+                                        <a href="https://www.linkedin.com/in/tiago-bertoldo-6021a61a8/"><AiFillLinkedin/></a>
+                                   </span>
+                                   <span>
+                                        <a href="https://github.com/sadberto"><AiFillGithub/></a>
+                                   </span>
+                              </div>
+                              <div className="container-info-header__text-apresentation">
+                                   <p>Hello World , je suis</p>
+                                   <p>Tiago Bertoldo</p>
+                                   <p>Front End Developpeur</p>
+                                   <div className="container-info-header__btn-download">
+                                   <ul>
+                                        <li>
+                                             <a href="#cv">Download CV</a>
+                                        </li>
+                                        <li>
+                                             <a href="cv">Contact</a>
+                                        </li>
+                                   </ul>
+                              </div>
+                              </div>
+                              
+                    </div>
+                    <div className="container-info-header__right">
+                              <figcaption>
+                                   <img src={imgProfil} alt="my de profil" />
+                              </figcaption>
+                    </div>
+              </section>
+
+          </div>
+       </header>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/Header';
-import '../GlobalStyle/variables.modules.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
