1f595c14319b11d6a316a6a4ecc216f315f6a0fd
Tiago Bertoldo <berto.hlb2@gmail.com>
2023-02-14 10:33:51 +0100

    correction url project 07

--- src/backend-jsx/projets.jsx
+++ src/backend-jsx/projets.jsx
@@ -88,7 +88,7 @@ export const infoProjet = [
             "Stocker des données de manière sécurisée",
             "Implémenter un modèle logique de données conformément à la réglementation",
         ],
-        "url" : "https://www.thbertoldo.tech/sauces",
+        "url" : "https://thbertoldo.tech/hot-takes/",
         "github" : "https://github.com/Tiago-Bertoldo/backend-pj-07",
         "urlImg" : "https://thbertoldo.tech/my_imgs/sauces.png",
         "altImg" : "Projet hot-takes creer en 2020",
