c9c34ad07b0f3057f6a52f50f9a78f8f1d74d29c
Tiago Bertoldo <berto.hlb2@gmail.com>
2023-02-20 10:07:49 +0100

    finish projet

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -5,12 +5,122 @@ import { BiUser } from 'react-icons/bi';
 import { AiFillLinkedin } from 'react-icons/ai';
 import { AiFillGithub } from 'react-icons/ai';
 import { BsFileEarmarkText } from 'react-icons/bs';
-import { useState } from 'react';
+import {AiOutlineCheckCircle} from 'react-icons/ai'
+import { useEffect, useState } from 'react';
+import emailJs from '@emailjs/browser'
 
 export default function Contact () {
+    let getSpan = document.querySelector('.loading-scroll')
+    const [userName , setName] = useState('')
+    const [userEmail , setEmail] = useState('')
+    const [userMsg, setMessage] = useState('')
+    const [isValid , setIsValid] = useState(false);
+    const [isValidButton , setIsValidButton] = useState(true) ;
+    const [isEffectActived , setEffectActived] = useState(false)
+    const [isSecurity , setIsSecurity] = useState({
+        validEmail : RegExp(
+            '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+         ),
+         validName : RegExp(
+            '[a-zA-Z0-9]+'
+         ),
+    })
 
-    return (
-        <footer id="contact">
+
+
+    const handleCheckInput = () => {
+        let errorName = document.querySelector('.error-name')
+        let errorEmail = document.querySelector('.error-email')
+        let errorMessage = document.querySelector('.error-message')
+        
+        if(userName.length >= 6 && isSecurity.validName.test(userName) ){
+            errorName.innerHTML = ''
+            setIsValid(true)
+        }else {
+            errorName.innerHTML = 'Minimum 6 characters *'
+        }
+
+        if(isSecurity.validEmail.test(userEmail) ){
+            errorEmail.innerHTML = ''
+            setIsValid(true)
+        }else {
+            errorEmail.innerHTML = 'Voulez regardez les champ email *'
+            setIsValid(false)
+        }
+        if(userMsg === ''){
+            errorMessage.innerHTML = 'Voulez regardez les champ message *'
+            setIsValid(false)
+        }else {
+            errorMessage.innerHTML = ''
+            setIsValid(true)           
+        }
+
+        
+        if(isValid) {
+            setIsValidButton(false)
+        }else {
+            setIsValidButton(true)
+        }
+        
+    }
+
+    useEffect(()=> {
+        if(isEffectActived){
+            handleCheckInput();
+        }
+        return;
+    })
+
+
+
+
+    const handleValidCheckEmail = (e) => {
+        e.preventDefault();
+        validedForm();
+        return;
+    }
+    
+     const setLoading = () => {
+        getSpan.classList.add('active-scroll');
+    }
+     const removeCarosel = () => {
+        getSpan.classList.remove('active-scroll');
+     }
+     const handleSetTimeOut = () => {
+        setTimeout(removeCarosel, 2000);
+     }
+
+     function validedForm (){
+        if(isValid){
+            const templateParams = {
+                from_name : userName ,
+                message : userMsg,
+                email : userEmail ,
+            }
+            emailJs.send('service_czb8ffl' , 'template_whrs8xf' , templateParams , 'erTyDLaYXqcLjq1Wx')
+            .then((res) => {
+                setLoading();
+            })
+            .then((res) => {
+                setEmail('')
+                setMessage('')
+                setName('')
+                setIsValid(false)
+                handleSetTimeOut();
+                setEffectActived(false);
+            })
+            .catch((errr) => {
+                console.log('error ' , errr)
+            })
+            return;
+        }
+
+        
+    }
+        
+        return (
+        <footer id="contact" >
+            <span className='loading-scroll'>Votre message a bien été envoyé.  <AiOutlineCheckCircle className='check-styles' /> </span>
            <section className="container-footer">
                 <div className="container-footer__header">
                     <h2>Contactez-moi</h2>
@@ -18,20 +128,37 @@ export default function Contact () {
                 </div>
                <div className="container-form-footer">
                     <aside className="container-form-footer__left">
-                       <form action="">
-                            <label htmlFor="user" className='label-style'>Nom:</label>
-                            <div className="style-input-icons">
-                                <label htmlFor="user"><BiUser/></label>
-                                <input type="text" name="user" id="user" data-icon='30' className='input-text-style'/>
+                       <form onSubmit={handleValidCheckEmail}>
+                            <label htmlFor="username" className='label-style'>Nom:</label>
+                            <div className="style-input-icons border-name">
+                                <label htmlFor="username"><BiUser/></label>
+                                <input type="text" name="username" id="username" data-icon='30' className='input-text-style'
+                                onChange={(e) => setName(e.target.value)}
+                                onClick={() => setEffectActived(true)}
+                                value={userName}
+                                />
                             </div>
+                            <span className="error-name"></span>
+                           
                             <label htmlFor="email">Email:</label>
                             <div className="style-input-icons">
                                 <label htmlFor="email"><MdAlternateEmail/></label>
-                                <input type="email" name="email" id="email" data-icon='30' className='input-text-style'/>
+                                <input type="email" name="email" id="email" data-icon='30' className='input-text-style border-email'
+                                onChange={(e) => setEmail(e.target.value)}
+                                onClick={() => setEffectActived(true)}
+                                value={userEmail}
+                                />
                             </div>
-                            <label htmlFor="subjet">Subjet:</label>
-                            <textarea name="subjet" id="subjet" cols="30" rows="10"></textarea>
-                            <input type="submit" value="ENVOYER" className='btn-submit'/>
+                            <span className="error-email"></span>
+                           
+                            <label htmlFor="subjet">Message:</label>
+                            <textarea name="subjet" id="subjet" cols="30" rows="10"  className='border-message' 
+                                onChange={(e) => setMessage(e.target.value)}
+                                onClick={() => setEffectActived(true)}
+                                value={userMsg}>
+                            </textarea>
+                            <span className="error-message"></span>
+                            <button className='btn-submit' type="submit" disabled={isValidButton}> ENVOYER</button>
                         </form> 
                     </aside>
                     <aside className="container-form-footer__right">
